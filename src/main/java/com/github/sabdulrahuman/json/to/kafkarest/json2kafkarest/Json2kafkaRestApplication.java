77bf3aa089a7efdafce979109b9d16a9dc89c449
SAbdulRahuman <abdul-rahuman.seeni-mohamed@hpe.com>
2020-05-14 17:17:32 +0530

    Initial Project setup

---
+++ src/main/java/com/github/sabdulrahuman/json/to/kafkarest/json2kafkarest/Json2kafkaRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.sabdulrahuman.json.to.kafkarest.json2kafkarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Json2kafkaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Json2kafkaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sabdulrahuman/json/to/kafkarest/json2kafkarest/Json2kafkaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sabdulrahuman.json.to.kafkarest.json2kafkarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Json2kafkaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
